46726358c73defbee47ce6e5c772ec2a0a6dc9e2
Tomás Sánchez Martínez <tomassanchezmartinez@MacBook-Pro-de-Tomas.local>
2022-04-19 21:08:59 +0200

    Added CardComponent files with the structure

---
+++ js/components/CardComponent.js
@@ -0,0 +1,58 @@
+class CardComponent {
+  element;
+
+  constructor(personaje) {
+    this.element = document.createElement("li");
+
+    this.element.className = "character col";
+
+    document.querySelector(".characters-list").append(this.element);
+
+    this.render(personaje);
+  }
+
+  render(personaje) {
+    this.element.innerHTML = `
+     <div class="card character__card">
+            <img
+              src="./img/${personaje.nombre}.jpg"
+              alt="Nombre y familia del personaje"
+              class="character__picture card-img-top"
+            />
+            <div class="card-body">
+              <h2 class="character__name card-title h4">${personaje.nombre} ${personaje.familia}</h2>
+              <div class="character__info">
+                <ul class="list-unstyled">
+                  <li>Edad: X años</li>
+                  <li>
+                    Estado:
+                    <i class="fas fa-thumbs-down"></i>
+                    <i class="fas fa-thumbs-up"></i>
+                  </li>
+                </ul>
+              </div>
+              <div class="character__overlay">
+                <ul class="list-unstyled">
+                  <li>Años de reinado: X</li>
+                  <li>Arma: XXX</li>
+                  <li>Destreza: X</li>
+                  <li>Peloteo: X</li>
+                  <li>Asesora a: X</li>
+                  <li>Sirve a: X</li>
+                </ul>
+                <div class="character__actions">
+                  <button class="character__action btn">habla</button>
+                  <button class="character__action btn">muere</button>
+                </div>
+              </div>
+            </div>
+            <i class="emoji"></i>
+          </div>
+        
+;
+  }
+    `
+         
+}
+
+export default CardComponent;
